1dacb9ed7cdc1fb6484a45dd5f72b341882bda69
Tony Samperi <antonino.samperi@crispybacon.it>
2020-09-10 16:28:42 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,62 +1,71 @@
 (() => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const links = document.querySelectorAll('.nav-links li');
-    const header = document.querySelector('header');
-    const txtName = document.querySelector('[name="firstName"]');
-    const txtSurname = document.querySelector('[name="lastName"]')
-    const txtEmail = document.querySelector('[name="email"]')
-    const textarea = document.getElementById('textarea');
-    const textareaCount = document.getElementById('textareaCount');
-    const sendMsgBtn = document.getElementById('send-btn');
-    let scrollValue = 0;
+	let lastScrollValue = 0;
+	const overlay = document.querySelector('.overlay');
+	const hamburger = document.querySelector('.hamburger');
+	const navLinks = document.querySelector('.nav-links');
+	const links = document.querySelectorAll('.nav-links li');
+	const header = document.querySelector('header');
+	const txtName = document.querySelector('[name="firstName"]');
+	const txtSurname = document.querySelector('[name="lastName"]')
+	const txtEmail = document.querySelector('[name="email"]')
+	const textarea = document.getElementById('textarea');
+	const textareaCount = document.getElementById('textareaCount');
+	const sendMsgBtn = document.getElementById('send-btn');
+	const scrollTrheshold = 24;
 
-    window.scroll({
-        top: 0
-    });
+	function countWord(val) {
+		return val ? val.length : 0;
+	}
 
-    window.addEventListener('scroll', () => {
-        headerOnScroll();
-    })
+	function resetForm() {
+		txtName.value = "";
+		txtSurname.value = "";
+		txtEmail.value = "";
+		textarea.value = "";
+	}
+	
+	// Like constructor, although you may have a real constructor if you use a real Class 
+	// (and yes, you can in plain JS)
+	const init = function(){
+		window.scroll({
+			top: 0
+		});
 
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('open');
-        hamburger.classList.toggle('open');
-        links.forEach(link => link.classList.toggle('fade'));
-    });
+		resetForm();
+		
+		window.addEventListener('scroll', () => {
+			// scrolling down
+			if (document.body.scrollTop > lastScrollValue || document.documentElement.scrollTop > lastScrollValue) {
+				header.classList.add('hidden');
+			}
+			else {				
+				header.classList.remove('hidden');
+			}
+			
+			lastScrollValue = window.scrollY;
+		});
+		
+		hamburger.addEventListener('click', () => {
+			document.body.classList.toggle('hide-overflow');
+			navLinks.classList.toggle('open');
+			hamburger.classList.toggle('open');
+			overlay.classList.toggle('show')
+			links.forEach(link => link.classList.toggle('fade'));
+		});
 
-    textarea.addEventListener('input', () => {
-        var txt = textarea.value;
-        var count = countWord(txt);
-        textareaCount.innerHTML = (count + '/600');
-    })
+		textarea.addEventListener('input', () => {
+			var txt = textarea.value;
+			var count = countWord(txt);
+			textareaCount.innerHTML = (count + '/600');
+		})
 
-    sendMsgBtn.addEventListener('click', () => {
-        if (txtName.value.length > 0 && txtSurname.value.length > 0 && txtEmail.value.length > 0 && textarea.value.length > 0) {
-            // resetForm();
-        }
-    });
+		sendMsgBtn.addEventListener('click', () => {
+			if (txtName.value.length>0 && txtSurname.value.length>0 && txtEmail.value.length>0 && textarea.value.length>0) {
+				// resetForm();
+			}
+		});
+	};
+	
+	init();
 
-    function headerOnScroll() {
-        if (document.body.scrollTop > scrollValue || document.documentElement.scrollTop > scrollValue) {
-            header.classList.remove('opening');
-            header.classList.add('closing');
-            scrollValue = window.scrollY;
-        } else {
-            header.classList.remove('closing');
-            header.classList.add('opening');
-            scrollValue = window.scrollY;
-        }
-    }
-
-    function countWord(val) {
-        return val ? val.length : 0;
-    }
-
-    function resetForm() {
-        txtName.value = "";
-        txtSurname.value = "";
-        txtEmail.value = "";
-        textarea.value = "";
-    }
 })();
